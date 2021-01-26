a557f65d55bff2a14bdd9b4a9a6cfe7eea4c02f3
zhoupeng <zhoupeng@xylink.com>
2021-01-26 16:06:42 +0800

    add spring

---
+++ camunda/camunda-simple/src/main/java/com/demo/spring/SpringProcessEngineTest.java
@@ -0,0 +1,22 @@
+package com.demo.spring;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+
+/**
+ * @author zhoupeng
+ */
+public class SpringProcessEngineTest {
+
+    public static void main(String[] args) {
+        //直接getDefaultProcessEngine会报错，Class<?> springConfigurationHelperClass = ReflectUtil.loadClass("org.camunda.bpm.engine.test.spring.SpringConfigurationHelper");
+        // 没有test包
+        //ProcessEngine defaultProcessEngine = ProcessEngines.getDefaultProcessEngine();
+        // System.out.println(defaultProcessEngine);
+
+        ProcessEngineConfiguration processEngineConfigurationFromResource = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti-context.xml");
+        ProcessEngine processEngine = processEngineConfigurationFromResource.buildProcessEngine();
+        System.out.println(processEngine);
+    }
+
+}
