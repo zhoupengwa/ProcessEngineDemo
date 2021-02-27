7f42f2a33526c9887be9a8bc2bde2d4e7afd7858
zhoupeng <zhoupeng@xylink.com>
2021-02-27 17:12:33 +0800

    update

--- camunda/camunda-simple/src/main/java/com/demo/ch10/TaskServiceTest.java
+++ camunda/camunda-simple/src/main/java/com/demo/ch10/TaskServiceTest.java
@@ -2,6 +2,8 @@ package com.demo.ch10;
 
 import org.camunda.bpm.engine.*;
 import org.camunda.bpm.engine.history.HistoricIdentityLinkLog;
+import org.camunda.bpm.engine.impl.persistence.entity.GroupEntity;
+import org.camunda.bpm.engine.impl.persistence.entity.UserEntity;
 import org.camunda.bpm.engine.repository.Deployment;
 import org.camunda.bpm.engine.repository.DeploymentBuilder;
 import org.camunda.bpm.engine.task.IdentityLink;
@@ -35,6 +37,40 @@ public class TaskServiceTest {
         taskService = processEngine.getTaskService();
         historyService = processEngine.getHistoryService();
         identityService = processEngine.getIdentityService();
+        //initGroupUser();
+    }
+
+    private void initGroupUser() {
+        GroupEntity groupEntity1=new GroupEntity();
+        groupEntity1.setRevision(0);
+        groupEntity1.setName("普通员工");
+        groupEntity1.setId("pt");
+        identityService.saveGroup(groupEntity1);
+
+        GroupEntity groupEntity2=new GroupEntity();
+        groupEntity2.setRevision(0);
+        groupEntity2.setName("总经理");
+        groupEntity2.setId("GeneralManager");
+        identityService.saveGroup(groupEntity2);
+
+        UserEntity userEntity1=new UserEntity();
+        userEntity1.setRevision(0);
+        userEntity1.setId("zs");
+        identityService.saveUser(userEntity1);
+
+        UserEntity userEntity2=new UserEntity();
+        userEntity2.setRevision(0);
+        userEntity2.setId("ls");
+        identityService.saveUser(userEntity2);
+
+        UserEntity userEntity3=new UserEntity();
+        userEntity3.setRevision(0);
+        userEntity3.setId("ww");
+        identityService.saveUser(userEntity3);
+
+        identityService.createMembership("zs","GeneralManager");
+        identityService.createMembership("ls","GeneralManager");
+        identityService.createMembership("ww","pt");
     }
 
 
@@ -49,7 +85,8 @@ public class TaskServiceTest {
 //                .addClasspathResource("com.demo/ch10/leave_var.bpmn")
 //                .addClasspathResource("com.demo/ch10/leave_listener.bpmn")
 //                .addClasspathResource("com.demo/ch10/leave_users.bpmn")
-                .addClasspathResource("com.demo/ch10/leave_candidateUsers.bpmn")
+//                .addClasspathResource("com.demo/ch10/leave_candidateUsers.bpmn")
+                .addClasspathResource("com.demo/ch10/leave_candidate_group.bpmn")
                 .deploy();
 
         System.out.println(deploymentBuilder);
@@ -60,7 +97,7 @@ public class TaskServiceTest {
     public void startProcessInstanceByKey() {
         Map<String, Object> variables = new HashMap<String, Object>();
 //        variables.put("userId", "张翠山");
-        variables.put("userId1", "帅哥1,帅哥2,帅哥3");
+//        variables.put("userId1", "帅哥1,帅哥2,帅哥3");
         runtimeService.startProcessInstanceByKey("leave", variables);
     }
 
@@ -185,5 +222,78 @@ public class TaskServiceTest {
         String taskId = "9208";
         String userId = "帅哥4";
         taskService.addCandidateUser(taskId, userId);
+    }
+
+
+    /**
+     * insert into ACT_HI_IDENTITYLINK ( ID_, TIMESTAMP_, TYPE_, USER_ID_, GROUP_ID_, TASK_ID_, ROOT_PROC_INST_ID_, PROC_DEF_ID_, OPERATION_TYPE_, ASSIGNER_ID_, PROC_DEF_KEY_, TENANT_ID_, REMOVAL_TIME_) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )
+     * <p>
+     * delete from ACT_RU_IDENTITYLINK where ID_ = ?
+     */
+    @Test
+    public void deleteCandidateUser() {
+        String taskId = "9208";
+        String userId = "帅哥4";
+        taskService.deleteCandidateUser(taskId, userId);
+    }
+
+
+    /**
+     * SELECT DISTINCT
+     * RES.REV_,RES.ID_,RES.NAME_,RES.PARENT_TASK_ID_,RES.DESCRIPTION_,RES.PRIORITY_,RES.CREATE_TIME_,RES.OWNER_,RES.ASSIGNEE_,RES.DELEGATION_,RES.EXECUTION_ID_,RES.PROC_INST_ID_,RES.PROC_DEF_ID_,RES.CASE_EXECUTION_ID_,RES.CASE_INST_ID_,RES.CASE_DEF_ID_,RES.TASK_DEF_KEY_,RES.DUE_DATE_,RES.FOLLOW_UP_DATE_,RES.SUSPENSION_STATE_,RES.TENANT_ID_
+     * FROM
+     * 	ACT_RU_TASK RES
+     * 	INNER JOIN ACT_RU_IDENTITYLINK I ON I.TASK_ID_ = RES.ID_
+     * WHERE
+     * 	(
+     * 		1 = 1
+     * 		AND (
+     * 			RES.ASSIGNEE_ IS NULL
+     * 			AND I.TYPE_ = 'candidate'
+     * 			AND ( I.GROUP_ID_ IN ( ? ) )
+     * 		)
+     * 	)
+     * ORDER BY
+     * 	RES.ID_ ASC
+     * 	LIMIT ? OFFSET ?
+     *
+     */
+    @Test
+    public void findGroupTask2() {
+        List<Task> taskList = taskService.createTaskQuery()
+                .taskCandidateGroup("pt")
+                .list();
+        for (Task task : taskList) {
+            System.out.println("#############");
+            System.out.println(task.getId());
+            System.out.println(task.getCreateTime());
+            System.out.println(task.getPriority());
+            System.out.println(task.getExecutionId());
+            System.out.println("#############");
+        }
+    }
+
+
+    /**
+     * select distinct RES.REV_, RES.ID_, RES.NAME_, RES.PARENT_TASK_ID_, RES.DESCRIPTION_,
+     * RES.PRIORITY_, RES.CREATE_TIME_, RES.OWNER_, RES.ASSIGNEE_, RES.DELEGATION_, RES.EXECUTION_ID_,
+     * RES.PROC_INST_ID_, RES.PROC_DEF_ID_, RES.CASE_EXECUTION_ID_, RES.CASE_INST_ID_, RES.CASE_DEF_ID_, RES.TASK_DEF_KEY_, RES.DUE_DATE_,
+     * RES.FOLLOW_UP_DATE_, RES.SUSPENSION_STATE_, RES.TENANT_ID_
+     * from ACT_RU_TASK RES inner join ACT_RU_IDENTITYLINK I on I.TASK_ID_ = RES.ID_
+     * WHERE ( 1 = 1 and ( RES.ASSIGNEE_ is null and I.TYPE_ = 'candidate' and ( I.USER_ID_ = ? or I.GROUP_ID_ IN ( ? ) ) ) ) order by RES.ID_ asc LIMIT ? OFFSET ?
+     */
+    @Test
+    public void findGroupTask3() {
+        List<Task> taskList = taskService.createTaskQuery()
+                .taskCandidateUser("ww") //I.USER_ID_ = ? or I.GROUP_ID_ IN ( ? )，查询候选人为自己的，与候选组包含自己的
+                .list();
+        for (Task task : taskList) {
+            System.out.println("#############");
+            System.out.println(task.getId());
+            System.out.println(task.getCreateTime());
+            System.out.println(task.getPriority());
+            System.out.println(task.getExecutionId());
+            System.out.println("#############");
+        }
     }
 }
